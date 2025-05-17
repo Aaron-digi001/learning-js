1891c7d75efc743251b4ad9343f001be2e46ccf2
Aaron-digi001 <jee.2024.air.100@gmail.com>
2025-05-17 13:11:42 +0000

    learned a lot

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+console.log("2" > 1); //. true
+console.log("02" > 1); //. true
+
+// console.log(null > 0); // false    ====> 0>0 false
+// console.log(null == 0); // false
+// console.log(null >= 0);     //true.  ====>. 0>=0 true.    it happens because of coercion
+
+// console.log(undefined == 0); // false
+// console.log(undefined > 0);   // false
+// console.log(undefined < 0);   // false
+
+// // ===    strictly equals too 
+
+// console.log("2" === 2);  // false
